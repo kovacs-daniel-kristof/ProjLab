4ddbc29a3dfe84e438b0ee58e896d91de05afdc3
danospeti <danospeti@gmail.com>
2017-04-17 20:36:35 +0200

    upc és specialrail v1

--- src/sheldon/SpecialRail.java
+++ src/sheldon/SpecialRail.java
@@ -1,30 +1,55 @@
 package sheldon;
 
 public class SpecialRail extends Rail{
-	public SpecialRail(){
-	}
-boolean isActive = false;
+	boolean isActive = false;
+	Rail[] originalRails = neighbours;
+	int nextRailIndex = 0;
 
-public void Activate (){ // kapu lesz belőle
-	System.out.println("SpecialRail has been activated!");
-	isActive = true;
-}
-public void Deactivate (){ // visszaáll special railba
-	System.out.println("SpecialRail has been deactivated!");
-	isActive = false;
-}
-public boolean getActive (){
-	System.out.println("SpecialRail is :" + isActive);
-	return isActive;
-}
-public void NoOtherTunnel (){
-	System.out.println("There's no tunnel");
-}
-public void ChangeGate (){
-	if(HasTrain()){
-		System.out.println("So i cannot be changed :/ - told the special rail to you, with a grim smile on its face");
-	}else{
-		System.out.println("Upcoming trains will face the other way from now on! - muhaha - says the special rail!");
+	public SpecialRail(){}
+
+	@Override
+	public Rail GetNextRail (Rail r){
+		if (!isActive)				//nincs alagút
+		{
+			if(originalRails[0] == r)
+				return originalRails[1];
+			else
+				return originalRails[0];
+		}
+		else						//van alagút
+		{
+			if (r == neighbours[0]) //jön ki az alagútból
+				return originalRails[nextRailIndex];
+			else
+				return neighbours[0];
+		}
 	}
-}
+	@Override
+	public void setNeighbours(Rail r1, Rail r2){
+		neighbours[0] = r1;
+		neighbours[1] = r2;
+		originalRails = neighbours;
+	}
+
+	public void Activate (){ // kapu lesz belĹ‘le
+		isActive = true;
+	}
+	public void Deactivate (){ // visszaĂˇll special railba
+		isActive = false;
+	}
+	public boolean getActive (){
+		return isActive;
+	}
+	public void NoOtherTunnel (){
+		// ez itt fölösleges
+	}
+	public void ChangeGate (){
+		if(!HasTrain())
+		{
+			if (nextRailIndex == 0)
+				nextRailIndex = 1;
+			else
+				nextRailIndex = 0;
+		}
+	}
 }
